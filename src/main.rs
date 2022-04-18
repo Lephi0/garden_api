d096d1fcda2cc13bb4559bbd2adb0f332b9c2c8d
Lephi0 <s.levente@gmail.com>
2022-04-18 10:00:57 +0200

    tokio schedule task and light switch logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
+use std::time::Duration;
 
+use tokio::{task, time};
 use once_cell::sync::Lazy;
 use reqwest::Error;
 use serde::{Serialize, Deserialize};
@@ -26,15 +28,35 @@ static CONFIG: Lazy<AppConfig> = Lazy::new(|| {
     config
 });
 
+static WAIT_FOR_SEC: u64 = 10;
+static LUX_THRESHOLD: u32 = 1000;
+
 #[tokio::main]
-async fn main() -> Result<(), Error> {
+async fn main() {
+    let forever = task::spawn(async {
+        let mut interval = time::interval(Duration::from_secs(WAIT_FOR_SEC));
+
+        loop {
+            let mut lux_value = 0;
+            interval.tick().await;
+            read_sensors(&mut lux_value).await;
+            toggle_led(&lux_value).await;
+        }
+    });
+
+    forever.await;
+}
+
+async fn read_sensors(lux_value: &mut u32) -> Result<(), Error> {
+    // TODO calling sensors should be a parameter value
+    // TODO .is_success() and error handling
     let sensor_list_response = client::get::<HashMap<String, Sensor>>(format!("/sensors")).await?;
 
     let (
         mut s_temp_id,
-        mut s_lux_id,
         mut s_hum_id,
         mut s_pres_id,
+        mut s_lux_id,
     ) = (None, None, None, None);
 
     // Get sensor ID-s
@@ -43,14 +65,14 @@ async fn main() -> Result<(), Error> {
             if sensor.r#type.as_ref().unwrap().contains("Temperature") {
                 s_temp_id = Some(id.to_string());
             }
-            if sensor.r#type.as_ref().unwrap().contains("LightLevel") {
-                s_lux_id = Some(id.to_string());
-            }
             if sensor.r#type.as_ref().unwrap().contains("Humidity") {
                 s_hum_id = Some(id.to_string());
             }
             if sensor.r#type.as_ref().unwrap().contains("Pressure") {
                 s_pres_id = Some(id.to_string());
+            }
+            if sensor.r#type.as_ref().unwrap().contains("LightLevel") {
+                s_lux_id = Some(id.to_string());
             }
         }
     }
@@ -63,16 +85,6 @@ async fn main() -> Result<(), Error> {
         let resp = temp_sensor.await?;
 
         println!("Temperature: {:#?} °C", trim_temp(resp.state.temperature.unwrap()));
-    }
-
-    // Light itensity
-    if s_lux_id.is_some() {
-        let s_lux_id = s_lux_id.unwrap();
-        //println!("Light sensor ID: {}", s_lux_id);
-        let lux_sensor = client::get::<Sensor>(format!("/sensors/{s_lux_id}"));
-        let resp = lux_sensor.await?;
-
-        println!("Light Level: {:#?} lux", resp.state.lux.unwrap());
     }
     
     // Humidity
@@ -95,27 +107,55 @@ async fn main() -> Result<(), Error> {
         println!("Pressure: {:#?} hPa", resp.state.pressure.unwrap());
     }
 
+    // Light itensity
+    if s_lux_id.is_some() {
+        let s_lux_id = s_lux_id.unwrap();
+        //println!("Light sensor ID: {}", s_lux_id);
+        let lux_sensor = client::get::<Sensor>(format!("/sensors/{s_lux_id}"));
+        let resp = lux_sensor.await?;
+
+        *lux_value = resp.state.lux.unwrap();
+        println!("Light Level: {:#?} lux", lux_value);
+    }
+    println!("----");
+
+    Ok(())
+}
+
+async fn toggle_led(lux_value: &u32) -> Result<(), Error> {
     // Get group "Garden"
-    // let groups_response = client::get::<HashMap<String, Group>>(format!("/groups")).await?;
-    // let mut group_id: Option<String> = None;
-    // // Get Garden group ID-s
-    // for (id, group) in &groups_response {
-    //     if group.name.contains("Garden") {
-    //         group_id = Some(id.to_string());
-    //     }
-    // }
-
+    let groups_response = client::get::<HashMap<String, Group>>(format!("/groups")).await?;
+    let mut group_id: Option<String> = None;
+    // Get Garden group ID-s
+    for (id, group) in &groups_response {
+        if group.name.contains("Garden") {
+            group_id = Some(id.to_string());
+        }
+    }
+    
     // Toggle group action
-    // if group_id.is_some() {
-    //     let group_id = group_id.unwrap();
-    //     println!("\n-----\nGroup ID: {}", group_id);
-    //     let mut action: HashMap<String, bool> = HashMap::new();
-    //     //action.insert("on", "false");
-    //     action.insert("toggle".to_string(), true);
+    if group_id.is_some() {
+        let group_id = group_id.unwrap();
+        let group_state = client::get::<Group>(format!("/groups/{group_id}")).await?;
+        
+        // Switch light only when it is not only in that state (if lux > 1000 and it is not already turned on)
+        if *lux_value > LUX_THRESHOLD && group_state.state.all_on == true {
+            switch_light_on(group_id, false).await?;
+        } else if *lux_value < LUX_THRESHOLD && group_state.state.all_on == false {
+            switch_light_on(group_id, true).await?;
+        }
+    }
 
-    //     let response = put(format!("/groups/{group_id}/action"), action).await?;
-    //     println!("Success: {:#?}", response.status().is_success());
-    // }
+    Ok(())
+}
+
+async fn switch_light_on(group_id: String, on: bool) -> Result<(), Error> {
+    println!(" - Switching: {} - ", on);
+    let mut action: HashMap<String, bool> = HashMap::new();
+    action.insert("on".to_string(), on);
+
+    let response = client::put(format!("/groups/{group_id}/action"), action).await?;
+    // println!("Success: {:#?}", response.status().is_success());
 
     Ok(())
 }
